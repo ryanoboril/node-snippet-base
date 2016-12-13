39d296c7c37ca55097ff5713aea775621ea6deea
Ryan O'Boril <ryan@arcweb.co>
2016-12-13 13:44:44 -0500

    added alograph backend as a code snippet to attempt subrouting with our backend server's controllers

--- index.js
+++ index.js
@@ -9,18 +9,20 @@
  * This can be more expressive, for example, you can use `fs` to
  * read all snippets in a directory and set them to your `module.exports`.
  */
+
 var BasicSnippet = require('./lib/basic_snippet');
-var AsyncSnippet = require('./lib/async_snippet');
 var CloudMineNode = require('cloudmine-servercode');
 // Require any other node module you want...
+
+var AlographBackend = require('./lib/backend_snippet')(CloudMineNode.server);
 
 /**
  * The `module.exports` **must** be called before the server is started,
  * or the server won't be able to read in the exports.
  */
 module.exports = {
+  backend: AlographBackend,
   basic: BasicSnippet,
-  async: AsyncSnippet
 };
 
 /**
